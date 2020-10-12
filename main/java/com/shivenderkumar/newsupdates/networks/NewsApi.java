fabbb0857a0768e6727fcaea9f3a931e0e435510
Shivender Kumar <41105346+shivenderkumar@users.noreply.github.com>
2020-10-12 14:02:05 +0530

    Add files via upload

---
+++ main/java/com/shivenderkumar/newsupdates/networks/NewsApi.java
@@ -0,0 +1,18 @@
+package com.shivenderkumar.newsupdates.networks;
+
+import com.google.gson.JsonObject;
+
+import org.json.JSONObject;
+
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface NewsApi {
+    String BASE_URL = "https://newsapi.org/v2/";
+    String yourAPIKEY = "";
+
+    @GET("top-headlines?country=in&apiKey="+yourAPIKEY)
+    Call<JsonObject> getNews();
+
+}
